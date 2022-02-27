e43c74f6ce193685387cce88096b359a8a5d25dd
sedabasaran <58148544+sedabasaran@users.noreply.github.com>
2022-02-28 01:36:40 +0300

    EmlakBurada-Jdbc/target-directory

--- EmlakBurada-Jdbc/target/EmlakBurada/EmlakBurada/src/main/java/emlakburada/repository/AdvertRepository.java
+++
@@ -1,9 +0,0 @@
-package emlakburada.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import emlakburada.model.Advert;
-
-public interface AdvertRepository extends JpaRepository<Advert, Integer> {
-
-}
